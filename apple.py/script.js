235c269915a6b529727b26e42d8848fb3b2f1d7a
temp192000 <codemonkey.ranjith@gmail.com>
2021-09-05 01:37:24 +0530

    feat: Game Scene

--- apple.py/script.js
+++ apple.py/script.js
@@ -1,12 +1,56 @@
 function init(){
     garden = document.getElementById('canvas-area');
-    snake = canvas.getContext('2d');
+    W = H= garden.width = garden.height = 600;
+    pen = garden.getContext('2d');
+    
+    cellSpace = 65;
+    
+    snake = {
+        body:[],
+        body_len: 5,
+        color: "blue",
+        direction: "right",
+        
+        createSnake(){
+            for(let i = snake.body_len; i > 0; i--){
+                snake.body.push({x:i,y:0});
+            }
+        },
+
+        drawSnake(){
+            pen.fillStyle = snake.color;
+            for(let i = 0; i < snake.body_len; i++){
+                pen.fillRect(snake.body[i].x * cellSpace, snake.body[i].y * cellSpace, cellSpace - 4, cellSpace - 2)
+            }
+        },
+
+        updateSnake(){
+            snake.body.pop();
+            tailX = snake.body[0].x;
+            tailY = snake.body[0].y;
+            snake.body.unshift({
+                x:tailX + 1,
+                y:tailY
+            })
+        }
+    }
+
+    snake.createSnake();
 }
 
-function draw(){}
+function draw(){
+    pen.clearRect(0, 0, W, H);
+    snake.drawSnake();
+}
 
-function update(){}
+function update(){
+    snake.updateSnake();
+}
 
-function gameLoop(){}
+function gameLoop(){
+    draw();
+    update();
+}
 
+init();
 gameState = setInterval(gameLoop, 100);
