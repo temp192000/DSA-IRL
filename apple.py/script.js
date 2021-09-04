8f80c5f95eff1a404b497910aedf52f2380c11ee
temp192000 <codemonkey.ranjith@gmail.com>
2021-09-04 22:05:21 +0530

    initial commit

---
+++ apple.py/script.js
@@ -0,0 +1,12 @@
+function init(){
+    garden = document.getElementById('canvas-area');
+    snake = canvas.getContext('2d');
+}
+
+function draw(){}
+
+function update(){}
+
+function gameLoop(){}
+
+gameState = setInterval(gameLoop, 100);
